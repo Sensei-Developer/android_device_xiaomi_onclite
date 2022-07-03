80f902f18b4e99f871580b0b6efa6d30f329eaeb
Galitsky Dmytro <melles.android@gmail.com>
2022-07-03 12:42:36 +0300

    Exodus-ify

--- lineage_onclite.mk
+++ lineage_onclite.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2019-2020 The LineageOS Project
+# Copyright (C) 2021-2022 The ExodusOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -15,6 +16,11 @@ $(call inherit-product, device/xiaomi/onclite/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Exodus Properties
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_AOSP_RECOVERY := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_onclite
 PRODUCT_DEVICE := onclite
@@ -23,9 +29,14 @@ PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 EXODUS_MAINTAINER := melles1991 & Sensei-Developer
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+# Fingerprint
+BUILD_FINGERPRINT := xiaomi/onc/onc:10/QKQ1.191008.001/V12.5.3.0.QFLCNXM:user/release-keys
+BUILD_DESCRIPTION := onc-user 10 QKQ1.191008.001 V12.5.3.0.QFLCNXM release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
